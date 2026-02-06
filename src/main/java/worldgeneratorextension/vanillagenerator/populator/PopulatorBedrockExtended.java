d0572079c19b03cf4477ad8d791790fec87670cb
Tripu1404 <121175509+Tripu1404@users.noreply.github.com>
2026-02-05 21:58:03 -0300

    Update PopulatorBedrockExtended.java

--- src/main/java/worldgeneratorextension/vanillagenerator/populator/PopulatorBedrockExtended.java
+++ src/main/java/worldgeneratorextension/vanillagenerator/populator/PopulatorBedrockExtended.java
@@ -2,29 +2,38 @@ package worldgeneratorextension.vanillagenerator.populator;
 
 import cn.nukkit.block.BlockID;
 import cn.nukkit.level.ChunkManager;
-import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.level.format.FullChunk; // CAMBIO: Usamos FullChunk
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 
-public class PopulatorBedrockExtended extends Populator {
+public class PopulatorDeepslate extends Populator {
     
+    // Si falla BlockID.DEEPSLATE usa el ID numérico correspondiente
+    private static final int DEEPSLATE_ID = BlockID.DEEPSLATE; 
+    private static final int STONE_ID = BlockID.STONE;
+
     private final int minHeight;
 
-    public PopulatorBedrockExtended(int minHeight) {
+    public PopulatorDeepslate(int minHeight) {
         this.minHeight = minHeight;
     }
 
     @Override
-    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, BaseFullChunk chunk) {
+    // CAMBIO: El último parámetro ahora es FullChunk
+    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                // Capa inferior sólida (ej: -64)
-                chunk.setBlockId(x, minHeight, z, BlockID.BEDROCK);
-
-                // Capas superiores aleatorias (ej: -63 a -60)
-                for (int i = 1; i < 5; i++) {
-                    if (random.nextBoundedInt(i + 1) == 0) {
-                        chunk.setBlockId(x, minHeight + i, z, BlockID.BEDROCK);
+                for (int y = 8; y >= this.minHeight; y--) {
+                    int blockId = chunk.getBlockId(x, y, z);
+                    
+                    if (blockId == STONE_ID) {
+                        if (y <= 0) {
+                            chunk.setBlockId(x, y, z, DEEPSLATE_ID);
+                        } else {
+                            if (random.nextFloat() < (8 - y) / 9.0f) {
+                                chunk.setBlockId(x, y, z, DEEPSLATE_ID);
+                            }
+                        }
                     }
                 }
             }
