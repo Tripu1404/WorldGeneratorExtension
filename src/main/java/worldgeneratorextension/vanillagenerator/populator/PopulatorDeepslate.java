521794611e828375daffdb5a5fa11856dff304aa
Tripu1404 <121175509+Tripu1404@users.noreply.github.com>
2026-02-05 21:57:34 -0300

    Update PopulatorDeepslate.java

--- src/main/java/worldgeneratorextension/vanillagenerator/populator/PopulatorDeepslate.java
+++ src/main/java/worldgeneratorextension/vanillagenerator/populator/PopulatorDeepslate.java
@@ -2,13 +2,13 @@ package worldgeneratorextension.vanillagenerator.populator;
 
 import cn.nukkit.block.BlockID;
 import cn.nukkit.level.ChunkManager;
-import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.level.format.FullChunk; // CAMBIO: Usamos FullChunk
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitRandom;
 
 public class PopulatorDeepslate extends Populator {
     
-    // Intenta usar el ID estático, si tu Nukkit es muy viejo y falla aquí, cámbialo por el ID numérico (ej. -6)
+    // Si falla BlockID.DEEPSLATE usa el ID numérico correspondiente
     private static final int DEEPSLATE_ID = BlockID.DEEPSLATE; 
     private static final int STONE_ID = BlockID.STONE;
 
@@ -19,22 +19,17 @@ public class PopulatorDeepslate extends Populator {
     }
 
     @Override
-    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, BaseFullChunk chunk) {
-        // Barrido del chunk
+    // CAMBIO: El último parámetro ahora es FullChunk
+    public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                // Iteramos desde Y=8 hacia abajo hasta la altura mínima
                 for (int y = 8; y >= this.minHeight; y--) {
                     int blockId = chunk.getBlockId(x, y, z);
                     
-                    // Solo reemplazamos Piedra normal
                     if (blockId == STONE_ID) {
                         if (y <= 0) {
-                            // Por debajo de 0, todo es Deepslate
                             chunk.setBlockId(x, y, z, DEEPSLATE_ID);
                         } else {
-                            // Entre 0 y 8, hacemos una mezcla (dithering)
-                            // Probabilidad de Deepslate aumenta cuanto más bajamos
                             if (random.nextFloat() < (8 - y) / 9.0f) {
                                 chunk.setBlockId(x, y, z, DEEPSLATE_ID);
                             }
